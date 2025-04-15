92893fb9ab77b7164497a138696ee2c63ae86d11
uri <uri.valevski@gmail.com>
2025-04-15 20:01:52 +0300

    initial-commit

---
+++ src/main.test.ts
@@ -0,0 +1,181 @@
+import {
+  datesInRange,
+  dateString,
+  dateToTimestamp,
+  dayInMs,
+  endOfDay,
+  endOfYear,
+  formatTime24Hour,
+  hourInMs,
+  humanTimeToTimestamp,
+  isNightTime,
+  msFromNightStart,
+  nextWeekday,
+  textHasDate,
+  timestampToHumanTime,
+  weekday,
+} from "./main.ts";
+
+import { assertEquals } from "jsr:@std/assert";
+
+Deno.test("conversions", () => {
+  assertEquals(humanTimeToTimestamp(-5, 2023, 11, 26, 22, 8), 1701054480000);
+});
+
+Deno.test("entire time display", () => {
+  assertEquals(
+    timestampToHumanTime("en", 2, 1701460680000),
+    "Friday, 21:58, December 1st 2023",
+  );
+});
+
+Deno.test("hour display", () => {
+  assertEquals(formatTime24Hour(-5, 1701460680000), "14:58");
+});
+
+Deno.test("parse in locale", () => {
+  assertEquals(
+    formatTime24Hour(2, dateToTimestamp(2, "2023-11-30 16:30")),
+    "16:30",
+  );
+});
+
+Deno.test("parse in locale", () => {
+  assertEquals(
+    formatTime24Hour(2, dateToTimestamp(2, "2024-03-25T18:00:00")),
+    "18:00",
+  );
+});
+
+Deno.test("non negative timestamp", () => {
+  assertEquals(dateToTimestamp(2, "2023-12-02"), 1701468000000);
+});
+
+Deno.test("T and Z in timestamp", () => {
+  assertEquals(dateToTimestamp(3, "2024-05-28T19:56:00Z"), 1716915360000);
+});
+
+Deno.test("correct timestamp", () => {
+  assertEquals(dateToTimestamp(2, "2023-12-15 22:00:00"), 1702670400000);
+});
+
+Deno.test("correct timestamp in another locale", () => {
+  assertEquals(dateToTimestamp(-5, "2023-12-15 15:00:00"), 1702670400000);
+});
+
+Deno.test("dates in range", () => {
+  assertEquals(
+    datesInRange("en", -5, {
+      start: 1702670400000,
+      end: 1702670400000 + 3 * dayInMs,
+    }),
+    ["Friday 15/12", "Saturday 16/12", "Sunday 17/12", "Monday 18/12"],
+  );
+  assertEquals(
+    datesInRange("en", 3, { start: 1734446160000, end: 1734532560000 }),
+    ["Tuesday 17/12", "Wednesday 18/12"],
+  );
+});
+
+Deno.test("end of day", () => {
+  const end = dateToTimestamp(7, "2023-12-15 00:00:00");
+  assertEquals(endOfDay(7, end - 12890471), end);
+});
+
+Deno.test("end of year", () => {
+  assertEquals(
+    endOfYear(7, dateToTimestamp(7, "2023-12-22 00:00:00")),
+    dateToTimestamp(7, "2024-01-01 00:00:00"),
+  );
+});
+
+Deno.test("weekday", () => {
+  assertEquals(weekday("en", 2, 1710799200000), "Tuesday");
+  assertEquals(weekday("he", 2, 1710799200000), "שלישי");
+});
+
+Deno.test("dateString", () => {
+  assertEquals(dateString(2, 1710799200000), "19/3");
+  assertEquals(dateString(-5, 1710283625000), "12/3");
+});
+
+Deno.test("ms from night start", () => {
+  assertEquals(
+    msFromNightStart(-5, humanTimeToTimestamp(-5, 2023, 11, 26, 2, 0)),
+    hourInMs * 5,
+  );
+});
+
+Deno.test("night time", () => {
+  assertEquals(
+    isNightTime(-5, humanTimeToTimestamp(-5, 2023, 11, 26, 22, 8)),
+    true,
+  );
+  assertEquals(
+    isNightTime(-2, humanTimeToTimestamp(-2, 2023, 11, 26, 3, 2)),
+    true,
+  );
+  assertEquals(
+    isNightTime(3, humanTimeToTimestamp(3, 2023, 11, 26, 19, 8)),
+    false,
+  );
+  assertEquals(
+    isNightTime(3, humanTimeToTimestamp(3, 2023, 11, 26, 14, 8)),
+    false,
+  );
+});
+
+Deno.test("text has date", () =>
+  [
+    "Facebook Log In חיפאית בתחתית - לוח אירועי תרבות ואמנות אלטרנטיביים בחיפה | למרות הכל ובגלל הכל , · 9.9K members Join group Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook אורלי פרץ Admin  ·  · Shared with Public group למרות הכל ובגלל הכל , הגיע הזמן לנגן , לשיר , לרקוד ולחגוג את החיים, נתראה בראשון 27/10          לאסקפיזם טהור החל מהשעה 20:30. מחכים לכם בwild jam, בואו להשמיע להישמע וליצור מוזיקה דרך חטיבת גולני 8, חיפה. All reactions: 1 Like Comment Share Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Facebook Log in or sign up for Facebook to connect with friends, family and people you know. Log In or Create new account",
+    `שבת שלום!
+שמחים לעדכן שעונת הקריוקי של ממתוק בנולה סוקס תפתח ביום ראשון הבא, 27.10 🕺💃 ותתקיים בכל ערב ראשון מהשעה 21:00.
+בנולה יש מרחב מוגן והשגחה פרטית ❤
+מזכירה שבקריוקי שלנו יש רשימת שירים, מי שמעוניין בשיר מסוים יכול לשלוח לנו הודעה.
+מחכים לכם עם הלב, המיקרופון והקהל האוהב ביותר.
+*27.10 יום ראשון 21:00 | נולה סוקס, שלום עליכם 4 מרכז זיו, חיפה*
+נתראה! 🧚`,
+  ].forEach((t) => assertEquals(textHasDate(t), true)));
+
+Deno.test("text has no date", () =>
+  [`Facebook Join or Log Into Facebook Sign Up This page isn't available The link you followed may be broken, or the page may have been removed. Go back to the previous page · Go to Feed · Visit our Help Center English (US) Español Français (France) 中文(简体) العربية Português (Brasil) Italiano 한국어 Deutsch हिन्दी 日本語 Sign Up Log In Messenger Facebook Lite Video Places Games Marketplace Meta Pay Meta Store Meta Quest Ray-Ban Meta Meta AI Instagram Threads Fundraisers Services Voting Information Center Privacy Policy Consumer Health Privacy Privacy Center Groups About Create ad Create Page Developers Careers Cookies Ad choices Terms Help Contact Uploading & Non-Users Settings Activity log Meta © 2024`]
+    .forEach((t) => assertEquals(textHasDate(t), false)));
+
+Deno.test("upcoming weekday", () => {
+  assertEquals(
+    nextWeekday(4, 2, humanTimeToTimestamp(2, 2024, 12, 3, 12, 49)),
+    humanTimeToTimestamp(2, 2024, 12, 5, 0, 0),
+  );
+  assertEquals(
+    nextWeekday(
+      0,
+      2,
+      humanTimeToTimestamp(2, 2024, 4, 12, 12, 18),
+    ),
+    humanTimeToTimestamp(2, 2024, 4, 14, 0, 0),
+  );
+  assertEquals(
+    nextWeekday(
+      0,
+      2,
+      humanTimeToTimestamp(2, 2024, 4, 13, 12, 18),
+    ),
+    humanTimeToTimestamp(2, 2024, 4, 14, 0, 0),
+  );
+  assertEquals(
+    nextWeekday(
+      4,
+      2,
+      humanTimeToTimestamp(2, 2023, 11, 26, 22, 45),
+    ),
+    humanTimeToTimestamp(2, 2023, 11, 30, 0, 0),
+  );
+  assertEquals(
+    nextWeekday(
+      0,
+      2,
+      humanTimeToTimestamp(2, 2023, 12, 2, 16, 40),
+    ),
+    humanTimeToTimestamp(2, 2023, 12, 3, 0, 0),
+  );
+});
---
+++ src/main.ts
@@ -0,0 +1,430 @@
+import { HebrewCalendar } from "npm:@hebcal/core";
+import { context } from "https://deno.land/x/context_inject@0.0.3/src/index.ts";
+import {
+  coerce,
+  concat,
+  join,
+  juxt,
+  letIn,
+  map,
+  min,
+  pipe,
+  smaller,
+  throwerCatcher,
+} from "https://deno.land/x/gamla@118.0.0/src/index.ts";
+import * as datetimeParse from "https://deno.land/std@0.214.0/datetime/parse.ts";
+import {
+  caseInsensitive,
+  simplify,
+  stringToRegexp,
+  wholeWord,
+} from "https://deno.land/x/silly_nlp@33.0.0/src/index.ts";
+
+const minuteInMs = 60 * 1000;
+
+export const hourInMs = 60 * minuteInMs;
+
+export const dayInMs = 24 * hourInMs;
+
+const numericalTimezoneToString = (tz: number) =>
+  `${
+    (Math.sign(tz) < 0 ? "-" : "+") +
+    Math.abs(tz).toString().padStart(2, "0")
+  }:00`;
+
+export const humanTimeToTimestamp = (
+  tz: number,
+  year: number,
+  month: number,
+  day: number,
+  hour: number,
+  minute: number,
+): number =>
+  Temporal.ZonedDateTime.from({
+    timeZone: numericalTimezoneToString(tz),
+    year,
+    month,
+    day,
+    hour,
+    minute,
+  }).epochMilliseconds;
+
+export const rangeOfNHours = (durationHours: number) =>
+  pipe(humanTimeToTimestamp, makeRange(hourInMs * durationHours));
+
+export type TimeRange = { start: number; end: number };
+
+const temporalDate = (tz: number, now: number) =>
+  Temporal.Instant.fromEpochMilliseconds(now).toZonedDateTimeISO(
+    numericalTimezoneToString(tz),
+  );
+
+const getHours = (d: Temporal.ZonedDateTime) =>
+  d.hour.toString().padStart(2, "0");
+
+const getMinutes = (d: Temporal.ZonedDateTime) =>
+  d.minute.toString().padStart(2, "0");
+
+const monthName = (language: Language, d: Temporal.ZonedDateTime) =>
+  monthNames[language][d.month - 1];
+
+const getOrdinalSuffix = (date: number): string => {
+  const lastTwoDigits = date % 100;
+  return lastTwoDigits > 3 && lastTwoDigits < 21
+    ? "th"
+    : { 1: "st", 2: "nd", 3: "rd" }[date % 10] || "th";
+};
+
+const dateDayString = (language: Language, d: Temporal.ZonedDateTime) =>
+  language === "en" ? `${d.day}${getOrdinalSuffix(d.day)}` : `ה-${d.day}`;
+
+export const timestampToMonthName = (
+  language: Language,
+  timezone: number,
+  timestamp: number,
+) => monthName(language, temporalDate(timezone, timestamp));
+
+const jsDateToDayOfWeek = (language: Language, d: Temporal.ZonedDateTime) =>
+  weekdayNames[language][d.dayOfWeek - 1];
+
+export const timestampToHumanTime = (
+  language: Language,
+  timezone: number,
+  timestamp: number,
+) =>
+  pipe(
+    temporalDate,
+    (d: Temporal.ZonedDateTime) =>
+      `${jsDateToDayOfWeek(language, d)}, ${jsDate24Clock(d)}, ${
+        monthName(language, d)
+      } ${dateDayString(language, d)} ${d.year}`,
+  )(timezone, timestamp);
+
+const jsDate24Clock = pipe(juxt(getHours, getMinutes), join(":"));
+
+export const formatTime24Hour = pipe(temporalDate, jsDate24Clock);
+
+export const dateString = pipe(
+  temporalDate,
+  (d: Temporal.ZonedDateTime) => `${d.day}/${d.month}`,
+);
+
+export const dateStringIncludingYear = pipe(
+  temporalDate,
+  (d: Temporal.ZonedDateTime) => `${d.day}/${d.month}/${d.year}`,
+);
+
+export const weekday = (language: Language, tz: number, time: number) =>
+  jsDateToDayOfWeek(language, temporalDate(tz, time));
+
+const { thrower: throwBadTimeString, catcher } = throwerCatcher();
+
+export const catchBadTimeString = catcher;
+
+const parseTimeStringHelper = (x: string) => {
+  for (
+    const format of [
+      "yyyy-MM-dd HH:mm:ss",
+      "yyyy-MM-dd HH:mm",
+      "yyyy-MM-dd",
+      "yyyy-MM-ddTHH:mm:ss",
+    ]
+  ) {
+    try {
+      return datetimeParse.parse(x, format);
+      // deno-lint-ignore no-empty
+    } catch (_) {}
+  }
+  throwBadTimeString();
+  throw new Error(); // for typing.
+};
+
+export const dateToTimestamp = (tz: number, text: string): number =>
+  letIn(
+    parseTimeStringHelper(text.replace(/[TZ]/g, " ").trim()),
+    (d) =>
+      Temporal.ZonedDateTime.from({
+        timeZone: numericalTimezoneToString(tz),
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        hour: d.getHours(),
+        minute: d.getMinutes(),
+      }).epochMilliseconds,
+  );
+
+type Language = "en" | "he";
+
+const weekdayNames = {
+  en: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+  he: [
+    "שני",
+    "שלישי",
+    "רביעי",
+    "חמישי",
+    "שישי",
+    "שבת",
+    "ראשון",
+  ],
+};
+
+const monthNames = {
+  en: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  he: [
+    "ינואר",
+    "פברואר",
+    "מרץ",
+    "אפריל",
+    "מאי",
+    "יוני",
+    "יולי",
+    "אוגוסט",
+    "ספטמבר",
+    "אוקטובר",
+    "נובמבר",
+    "דצמבר",
+  ],
+};
+
+export const nowTimestamp = () => new Date().getTime();
+
+export const endOfDay = (tz: number, timestamp: number): number =>
+  temporalDate(tz, timestamp + 24 * hourInMs).startOfDay().epochMilliseconds;
+
+export const dayStart = (tz: number, timestamp: number): number =>
+  temporalDate(tz, timestamp).startOfDay().epochMilliseconds;
+
+export const endOfYear = (tz: number, timestamp: number): number =>
+  letIn(
+    temporalDate(tz, timestamp),
+    (current) =>
+      Temporal.ZonedDateTime.from({
+        timeZone: numericalTimezoneToString(tz),
+        year: current.year + 1,
+        month: 1,
+        day: 1,
+      }).epochMilliseconds,
+  );
+
+export const datesInRange = (
+  language: Language,
+  tz: number,
+  { start, end }: TimeRange,
+) => {
+  const result = [];
+  let current = start;
+  while (current < end + dayInMs) {
+    result.push(`${weekday(language, tz, current)} ${dateString(tz, current)}`);
+    current += dayInMs;
+  }
+  return result;
+};
+
+export const currentYear = pipe(
+  temporalDate,
+  ({ year }: Temporal.ZonedDateTime) => year,
+);
+
+export const intersectTimeRanges = (r1: TimeRange, r2: TimeRange) => ({
+  start: Math.max(r1.start, r2.start),
+  end: Math.min(r1.end, r2.end),
+});
+
+const monthAcronyms = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const textHasDate = (text: string) =>
+  [
+    ...concat(Object.values(monthNames)),
+    ...monthAcronyms,
+  ]
+    // "May" is ambiguous.
+    .filter((x) => x.toLocaleLowerCase() !== "may")
+    .map(simplify)
+    .map(pipe(stringToRegexp, caseInsensitive, wholeWord))
+    .some((r) => r.test(text)) || checkHasDate(text);
+
+const checkHasDate = (input: string) =>
+  [
+    /\b\d{4}-\d{2}-\d{2}\b/, // YYYY-MM-DD
+    /\b\d{2}-\d{2}-\d{4}\b/, // DD-MM-YYYY
+    /\b\d{2}\/\d{2}\/\d{4}\b/, // MM/DD/YYYY
+    /\b\d{4}\/\d{2}\/\d{2}\b/, // YYYY/MM/DD
+    /\b\d{1,2}[./]\d{1,2}\b/, // without the year
+  ].some((x) => x.test(input));
+
+export const { access: contextTimezone, inject: injectTimezone } = context(
+  (): number => {
+    throw new Error("timezone was not provided");
+  },
+);
+
+export const { access: contextTimestamp, inject: injectTimestamp } = context(
+  (): number => {
+    throw new Error("no time was injected");
+  },
+);
+
+const nightStart = 21 * hourInMs;
+
+const nightEnd = 5 * hourInMs;
+
+export const nightDuration = nightEnd + hourInMs * 24 - nightStart;
+
+export const msFromNightStart = pipe(
+  temporalDate,
+  (x: Temporal.ZonedDateTime) =>
+    x.epochMilliseconds - x.startOfDay().epochMilliseconds,
+  (ms: number) =>
+    ms > nightStart ? ms - nightStart : ms + (24 * hourInMs - nightStart),
+);
+
+export const isNightTime = pipe(msFromNightStart, smaller(nightDuration));
+
+export const makeRange = (duration: number) => (start: number): TimeRange => ({
+  start,
+  end: start + duration,
+});
+
+export const nextWeekday = (
+  desiredDayOfWeek: number, // e.g. Thursday will be 4
+  tz: number,
+  timestamp: number,
+): number =>
+  letIn(
+    temporalDate(tz, timestamp),
+    ({ dayOfWeek }) => {
+      return dayStart(
+        tz,
+        timestamp + (dayInMs * (desiredDayOfWeek >= dayOfWeek
+          ? desiredDayOfWeek - dayOfWeek
+          : 7 - dayOfWeek + desiredDayOfWeek)),
+      );
+    },
+  );
+
+export const lastWeekday = (
+  desiredDayOfWeek: number, // e.g. Thursday will be 4
+  tz: number,
+  timestamp: number,
+): number =>
+  letIn(
+    temporalDate(tz, timestamp),
+    ({ dayOfWeek }) => {
+      return dayStart(
+        tz,
+        timestamp + (dayInMs * (desiredDayOfWeek >= dayOfWeek
+          ? -7 + desiredDayOfWeek - dayOfWeek
+          : desiredDayOfWeek - dayOfWeek)),
+      );
+    },
+  );
+
+const timeOpt = (
+  exclusive: boolean,
+  defaultTime: string,
+  year: number,
+  datetime: string,
+) => {
+  const [date, time] = datetime.split(" ");
+  const [monthStr, dayStr] = date.split("-");
+  const day = Number.parseInt(dayStr);
+  const month =
+    monthNames.en.findIndex((m) =>
+      m.toLowerCase().includes(monthStr.toLowerCase())
+    ) + 1;
+  if (month === 0) throwBadTimeString();
+  const fixedDay = (year % 4 !== 0 && month === 2 && day === 29) ? 28 : day;
+  return dateToTimestamp(
+    contextTimezone(),
+    `${
+      [
+        year,
+        month.toString().padStart(2, "0"),
+        fixedDay.toString().padStart(2, "0"),
+      ].join("-")
+    } ${time ?? defaultTime}`.trim(),
+  ) + ((!time && !exclusive) ? dayInMs : 0);
+};
+
+export const parseTimeWithoutYear = (
+  exclusive: boolean,
+  defaultTime: string,
+  datetime: string,
+  relativeTo: number,
+) =>
+  pipe(
+    map((x: number) =>
+      timeOpt(
+        exclusive,
+        defaultTime,
+        currentYear(contextTimezone(), relativeTo) + x,
+        datetime,
+      )
+    ),
+    min((x: number) => Math.abs(x - relativeTo)),
+  )([0, 1, -1]);
+
+const jsDateToStartOfDayInMs = (date: Date): number => {
+  const dateInContextTz = new Date(
+    date.getTime() + date.getTimezoneOffset() * 60 * 1000 + contextTimezone(),
+  );
+  return dateInContextTz.setHours(0, 0, 0, 0);
+};
+
+export const israeliHoliday = (name: string, days: number) =>
+  pipe(
+    (tz: number, now: number): Date =>
+      coerce(
+        HebrewCalendar.calendar({
+          start: new Date(now),
+          end: new Date(Math.max(now + dayInMs * 90, endOfYear(tz, now))),
+        }).find(({ desc }) => desc === name)?.date.greg(),
+      ),
+    jsDateToStartOfDayInMs,
+    (start: number): TimeRange => ({
+      start: start + 12 * hourInMs,
+      end: endOfDay(contextTimezone(), start) + (days - 1) * dayInMs,
+    }),
+  );
+
+export const israelTimezone = (): number => {
+  const now = new Date();
+  const intlOptions = { timeZone: "Asia/Jerusalem", hour12: false };
+  const israelTime = new Intl.DateTimeFormat("en-US", intlOptions).format(now);
+  const israelDate = new Date(`${now.toDateString()}T${israelTime}`);
+  return israelDate.getUTCHours() - now.getUTCHours() === 3 ? 3 : 2;
+};
